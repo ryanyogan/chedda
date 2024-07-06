ae29d840ce61cca1d9dfc2dbd551f5f4be80825f
Ryan Yogan <ryanyogan@gmail.com>
2024-07-06 17:41:52 -0500

    dev // show loading skeleton

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -3,12 +3,34 @@
 import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { columns } from "./columns";
 
 export default function AccountsPage() {
   let newAccount = useNewAccount();
+  let accountsQuery = useGetAccounts();
+  let accounts = accountsQuery.data || [];
+  let isDisabled = accountsQuery.isLoading;
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -24,10 +46,8 @@ export default function AccountsPage() {
           <DataTable
             onDelete={() => {}}
             filterKey="name"
-            data={[
-              { id: "1", name: "Account 1" },
-              { id: "2", name: "Account 2" },
-            ]}
+            disabled={isDisabled}
+            data={accounts}
             columns={columns}
           />
         </CardContent>
